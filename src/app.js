cce3b640b4bcb5f3606efd496f16b83a5675b781
thesammet <thesammet@gmail.com>
2024-11-24 01:53:04 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('../src/db/mongoose')
 const userRouter = require('../src/routers/user')
 const categoryRouter = require('../src/routers/category')
+const templateRouter = require('../src/routers/template')
 const app = express()
 
 app.use(express.json())
