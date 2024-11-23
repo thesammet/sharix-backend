cb3635e509ef0ca1612c4ab083afa4cc0c493397
thesammet <thesammet@gmail.com>
2024-11-24 01:10:12 +0300

    Update category.js

--- src/models/category.js
+++ src/models/category.js
@@ -15,7 +15,6 @@ const categorySchema = new mongoose.Schema({
     },
     slug: {
         type: String,
-        required: true,
         unique: true,
         lowercase: true,
         trim: true,
