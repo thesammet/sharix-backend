aa33434c8497d7c3db75284e6d873a6fb8228bbe
thesammet <thesammet@gmail.com>
2024-11-24 13:51:57 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ require('../src/db/mongoose')
 const userRouter = require('../src/routers/user')
 const categoryRouter = require('../src/routers/category')
 const templateRouter = require('../src/routers/template')
+const messageRouter = require('../src/routers/message')
 const app = express()
 
 app.use(express.json())
 app.use(userRouter)
 app.use(categoryRouter)
 app.use(templateRouter)
+app.use(messageRouter)
 app.set('trust proxy', 1);
 
 module.exports = app
