d0d7dd3c56b4d4ea764013ee53fc62a982eced2e
thesammet <thesammet@gmail.com>
2024-11-26 04:24:36 +0300

    Update background_category.js

--- src/routers/background_category.js
+++ src/routers/background_category.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const BackgroundCategory = require('../models/backgroundCategory');
+const BackgroundCategory = require('../models/background_category');
 const router = express.Router();
 const { successResponse, errorResponse } = require('../utils/response');
 
